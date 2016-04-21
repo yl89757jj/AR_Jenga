0bc5485f850acc813d108844a126b1a35980fb05
yl89757jj <yanglu930309@gmail.com>
2016-04-20 22:51:25 -0400

    with selection setting

---
+++ Assets/Script/Brick.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Brick : MonoBehaviour {
+	public bool selectable;
+
+	// Use this for initialization
+	void Start () {
+		selectable = false;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
